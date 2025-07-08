9c04654ae41a191640482bbb12064124535a292d
lxng <lxng@example.org>
2025-07-08 10:05:46 +0700

    add

---
+++ 17_spring_boot/2-handler_controller/src/test/java/com/codegym/handler_controller/HandlerControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.handler_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlerControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
